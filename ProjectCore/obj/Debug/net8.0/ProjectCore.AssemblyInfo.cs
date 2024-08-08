e6874befcc46e9edf3987ec205bb011ee35c4a7e
Michiel-DeBruyne <75170820+Michiel-DeBruyne@users.noreply.github.com>
2024-08-08 16:15:10 +0200

    removed_photos folder

--- ProjectCore/obj/Debug/net8.0/ProjectCore.AssemblyInfo.cs
+++ ProjectCore/obj/Debug/net8.0/ProjectCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcdedae6797869dcca76402c8b14e9b400cf092b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4806ae83bab869bb255778296769b30137331441")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
