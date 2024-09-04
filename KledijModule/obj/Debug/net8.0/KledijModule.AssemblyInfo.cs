b96371ee80d652547eb8908a9335711494c51aed
Michiel-DeBruyne <Michiel.DeBruyne@police.belgium.eu>
2024-09-04 22:58:04 +0200

    - moved logout to under user profile

--- KledijModule/obj/Debug/net8.0/KledijModule.AssemblyInfo.cs
+++ KledijModule/obj/Debug/net8.0/KledijModule.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KledijModule")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03fcabc21a09a605714003c3dc6df737b564dc95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd262f696d628b1d55e1f08b835cf67adf5cd9fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("KledijModule")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KledijModule")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
