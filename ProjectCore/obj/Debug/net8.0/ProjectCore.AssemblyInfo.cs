4d5733068f6adb6cc64186530c2f379ccc471ff2
Michiel-DeBruyne <Michiel.DeBruyne@police.belgium.eu>
2024-09-06 08:34:30 +0200

    - data-bs-collapse added to category navigation - card footer blijft beneden bij productcards

--- ProjectCore/obj/Debug/net8.0/ProjectCore.AssemblyInfo.cs
+++ ProjectCore/obj/Debug/net8.0/ProjectCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b96371ee80d652547eb8908a9335711494c51aed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aa01c3909e07a48cdd3e47365ba68a34ab09d9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
